f4e3f82a8a566bafe82dea928c623adc660365d4
Kevin Fu <kevin.fu808@gmail.com>
2022-01-18 00:12:57 -0800

    76/81 completed - 94%

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -33,10 +33,19 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        if s.len() == 0 {
+            Person::default()
+        } else {
+            s.split_once(",").map_or_else(|| Person::default(), |(name, age)| {
+                if name.len() == 0 {
+                    Person::default()
+                } else {
+                    age.parse::<usize>().map_or(Person::default(), |parsedAge| Person { name: name.to_string(), age: parsedAge })
+                }
+            })
+        }
     }
 }
 
